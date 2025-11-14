d6a3f8c0e80c78ee95295da9ccbc3e952feaff95
Newiq <784751939@qq.com>
2025-11-14 14:24:49 -0500

    test of api in client

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,16 @@ builder.Services.AddDbContext<API.Data.AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+          .AllowAnyMethod()
+          .AllowAnyHeader());
 
 app.MapControllers();
 
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,21 +1,22 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit{
   private http = inject(HttpClient);
-  protected readonly title = signal('client');
+  protected readonly title = 'Matchly';
+  protected members = signal<any>([]);
+  
 
   ngOnInit(): void {
     this.http.get<{ title: string }>('http://localhost:5001/api/members').subscribe({
       next: (data) => {
-        this.title.set(data.title);
+        this.members.set(data);
       },
       error: (err) => {
         console.error('Failed to fetch app info', err);
