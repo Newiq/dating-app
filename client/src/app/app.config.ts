e391048821776bf08041e59749c157ee1b1c9905
Newiq <784751939@qq.com>
2025-11-25 16:28:47 -0500

    routes guards

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { InitService } from '../core/service/init-service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter(routes,withViewTransitions()),
+    provideHttpClient(),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+      return new Promise<void>((resolve) => {
+        setTimeout(async () => {
+          try {
+            await lastValueFrom(initService.init());
+          } finally {
+            // Any cleanup or finalization logic can go here if needed
+            const splash = document.getElementById('init-splash');
+            if (splash) {
+              splash.remove();
+            }
+            resolve();
+          }
+        }, 500); // Simulate a delay for demonstration purposes
+      });
+    }),
   ]
 };
---
+++ client/src/core/guards/auth-guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../service/account-service';
+import { inject } from '@angular/core';
+import { ToastService } from '../service/toast-service';
+
+export const authGuard: CanActivateFn = () => {
+  const accountService = inject(AccountService);
+  const toast = inject(ToastService);
+  if (accountService.currentUser()) {
+    return true;
+  } else {
+    toast.error('You shall not pass! Please log in to access this page.');
+    return false;
+  }
+};
